df7e3c1d4ba67bcb260771d8d2259c0b46f7742e
omarcossantiago <marcos-propaganda@hotmail.com>
2025-05-06 23:15:48 -0300

    Update menu.php

--- menu.php
+++ menu.php
@@ -1,5 +1,5 @@
 <div class="menu">
-    <a href="index.html"><button>Home</button></a>
+    <a href="index.php"><button>Home</button></a>
     <a href="cadastrar_aluno.php"><button>Cadastrar Aluno</button></a>
     <a href="cadastrar_refeicao.php"><button>Cadastrar Refeição</button></a>
     <a href="reserva_refeicao.php"><button>Reservar Refeição</button></a>
