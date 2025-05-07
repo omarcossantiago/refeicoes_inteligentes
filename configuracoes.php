a08bb09c4de6c6a874e49af8423f2e35d319eb8f
omarcossantiago <marcos-propaganda@hotmail.com>
2025-05-06 23:18:28 -0300

    Update configuracoes.php

--- configuracoes.php
+++ configuracoes.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Configurações - Refeições Inteligentes</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="style.css">
     <script>
     function confirmarExclusao(tipo) {
         return confirm('Tem certeza que deseja excluir este ' + tipo + '? Essa ação não pode ser desfeita.');
